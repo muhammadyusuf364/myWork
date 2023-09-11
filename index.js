f1d93c4c92c9ebb11bbaea87c2a8ccadce9a32a7
muhammadyusuf364 <mirzayevmuhammadyusuf359@gmail.com>
2023-09-11 14:42:01 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+var typed = new Typed(".text", {
+  strings: ["Frontend Developer !"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
+const menu = document.querySelector(".menu");
+const menux = document.querySelector(".menu-x");
+const respons = document.querySelector(".respons");
+
+menu.addEventListener("click", () => {
+  respons.classList.toggle("hidden");
+  menux.classList.toggle("hidden");
+});
